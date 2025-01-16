4130bb878575e6b246576095c01ce656f0bb6edc
Anastasiastasia <nastya.lalaloopsy@gmail.com>
2025-01-16 21:20:30 +0300

    Update Main.cpp

--- Lab5Os/Main.cpp
+++ Lab5Os/Main.cpp
@@ -1,17 +1,4 @@
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <windows.h>
-using namespace std;
-const wchar_t* REQUEST_PIPE = L"\\\\.\\pipe\\request_pipe"; 
-const wchar_t* RESPONSE_PIPE = L"\\\\.\\pipe\\response_pipe"; 
-const wchar_t* FILENAME = L"employees.bin"; 
-struct employee {
-    int num;               
-    char name[10];        
-    double hours;        
-};
-mutex fileMutex; 
+#include "Header.h"
 void createEmployeeFile() {
     HANDLE hFile = CreateFile(
         FILENAME,
