9f40b315d40b2606bb6d618c1581e8a624c6d483
Anastasiastasia <nastya.lalaloopsy@gmail.com>
2025-01-16 21:21:16 +0300

    Update Test.cpp

--- Lab5Os/Test.cpp
+++ Lab5Os/Test.cpp
@@ -1,22 +1,5 @@
 #include <gtest/gtest.h>
-#include <windows.h>
-#include <thread>
-#include <mutex>
-#include <iostream>
-using namespace std;
-const wchar_t* REQUEST_PIPE = L"\\\\.\\pipe\\request_pipe";
-const wchar_t* RESPONSE_PIPE = L"\\\\.\\pipe\\response_pipe";
-const wchar_t* FILENAME = L"employees.bin";
-
-struct employee {
-    int num;
-    char name[10];
-    double hours;
-};
-mutex fileMutex;
-void createEmployeeFile();
-void handleClientRequests();
-void requestEmployeeRecord();
+#include "Header.h"
 TEST(EmployeeTest, CreateEmployeeFile) {
     DeleteFile(FILENAME);
     thread t(createEmployeeFile);
