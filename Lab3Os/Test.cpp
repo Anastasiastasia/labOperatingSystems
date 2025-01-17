5b44ca8477d12960032958a056913dce731e4a22
Anastasiastasia <nastya.lalaloopsy@gmail.com>
2025-01-17 07:54:19 +0300

    Update Test.cpp

--- Lab3Os/Test.cpp
+++ Lab3Os/Test.cpp
@@ -1,28 +1,5 @@
 #include <gtest/gtest.h>
-#include <windows.h>
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <iostream>
-
-using namespace std;
-
-const int MAX_MARKERS = 10;
-const int SLEEP_DURATION = 5; 
-CRITICAL_SECTION cs;
-HANDLE hEventStart; 
-HANDLE hEventContinue; 
-HANDLE hEventStop; 
-
-struct Marker {
-    int id;
-    int markedCount;
-    bool canContinue;
-    bool stopRequested;
-};
-
-vector<int> numbers;
-vector<Marker> markers;
+#include "Header.h"
 
 DWORD WINAPI markerThread(LPVOID param) {
     Marker& marker = *static_cast<Marker*>(param);
